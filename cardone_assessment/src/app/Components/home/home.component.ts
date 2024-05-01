67238496967aaf937cc952e51a6b46b6cc235a26
GuillermoJ-smbs <guillermo.jimenez@smbssolutions.com>
2024-05-01 04:26:35 -0700

    Playbook section

--- cardone_assessment/src/app/Components/home/home.component.ts
+++ cardone_assessment/src/app/Components/home/home.component.ts
@@ -2,15 +2,20 @@ import { Component } from '@angular/core';
 import { VideoSectionComponent } from '../video-section/video-section.component';
 import { CountdownComponent } from '../countdown/countdown.component';
 import { PlaybookCtaComponent } from '../playbook-cta/playbook-cta.component';
-
+import { TestimonialsComponent } from '../testimonials/testimonials.component';
+import { PlaybookComponent } from '../playbook/playbook.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [VideoSectionComponent, CountdownComponent, PlaybookCtaComponent],
+  imports: [
+    VideoSectionComponent,
+    CountdownComponent,
+    PlaybookCtaComponent,
+    TestimonialsComponent,
+    PlaybookComponent,
+  ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
