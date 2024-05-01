4946aa38930a2b7630dd986ee7550c7e4f0503e8
GuillermoJ-smbs <guillermo.jimenez@smbssolutions.com>
2024-04-30 20:45:10 -0700

    Module setup

--- cardone_assessment/src/app/app.config.ts
+++ cardone_assessment/src/app/app.config.ts
@@ -2,6 +2,7 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { HomeComponent } from './Components/home/home.component';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes)]
