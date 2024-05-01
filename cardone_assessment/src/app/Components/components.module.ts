5a23a00adf7759dd2d538d3f7df225a730b66009
GuillermoJ-smbs <guillermo.jimenez@smbssolutions.com>
2024-05-01 00:47:44 -0700

    Video section

--- cardone_assessment/src/app/Components/components.module.ts
+++ cardone_assessment/src/app/Components/components.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
+import { VideoSectionComponent } from './video-section/video-section.component';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    HomeComponent
+    HomeComponent,
+    VideoSectionComponent
   ],
   exports: [
-    HomeComponent
+    HomeComponent,
   ]
 })
 export class ComponentsModule{ }
--- cardone_assessment/src/app/Components/home/home.component.ts
+++ cardone_assessment/src/app/Components/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { VideoSectionComponent } from '../video-section/video-section.component';
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [VideoSectionComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
